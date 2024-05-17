04a0a5e84c27629fbbc616dd5b0328416eb63525
lince <108680694+lince26@users.noreply.github.com>
2024-05-16 23:45:43 -0300

    Update solver_AWrongPcs.py

--- solver_AWrongPcs.py
+++ solver_AWrongPcs.py
@@ -1,12 +1,13 @@
 import copy
 from auxiliary_functions import turn_grid_to_tuple, turn_tuple_to_grid
 from settings import *
+#PRIMEIRO DE TUDO ESSA RUMA DE PRINT DE BICHO ERA PRA SABER ONDE TAVA ERRADO
 
 #começa criando a classe do tipo de resolução
 class AWrongPcs_solver():
 
     #gerar os movimentos possiveis para a casa vazia
-    def generates_moves(self, empty_pos):
+    def generate_moves(self, empty_pos):
         possible_moves = []
 
         # para cima
@@ -53,9 +54,12 @@ class AWrongPcs_solver():
             path.append(turn_tuple_to_grid(current_state))
             current_state = parents_dict.get(current_state)
         
-        # retorna os caminho do estado inicial até o final
+        # retorna os caminho do estado inicial até o final]
+        print(path)
         return path[::-1]
         
+
+    
     def count_wrong_pieces(self, state, objective_state):
     # contar o numero de peças fora do lugar
         wrong_pieces = 0
@@ -63,48 +67,58 @@ class AWrongPcs_solver():
             if tile != 0 and tile != objective_state[i]:
                 wrong_pieces += 1
             return wrong_pieces
-    
-    # extrair a prioridade de um item dentro do open_set
-    def priority_fucntion(item):
-        return item[1]
-    
-
-    
+        print(wrong_pieces)
+        
     def solve_A_WrongPcs(self, initial_state, objective_state):
-        #cria umconjunto para armazenar os estados que vão ser explorados
-        open_set = {turn_grid_to_tuple(initial_state): self.count_wrong_pieces(initial_state)+0}
+        open_set = {turn_grid_to_tuple(initial_state): self.count_wrong_pieces(initial_state, objective_state)+0}
+        #open_set = {turn_grid_to_tuple(initial_state): self.count_wrong_pieces(initial_state)}
         closet_set = set()
-        #mapear cada estado explorado
         parents_dict = {}
 
-        #loop principal
+        print("girafa")
         while open_set:
-            for item in open_set.items():
-                if not current_state or priority > item[1]:
-                    current_state, priority = item
 
+            current_state, priority = min(open_set.items(), key=lambda x: x[1])
             current_state = turn_grid_to_tuple(current_state)
-            
-            #Se o estado atual for igual ao estado ideal confirma
+
             if current_state== objective_state:
                 return self.resconstruct_path(current_state, parents_dict)
-                
-            #remove o estado atual, pois já foi explorado e adiciona a closet_set
+            
             open_set.pop(turn_grid_to_tuple(current_state))
             closet_set.add(turn_grid_to_tuple(current_state))
 
-            # gera sucessores a partir da posição atual e a do zero e vai por todos os estados proximos possiveis
             for next_state in self.generate_states(current_state, self.find_zero_pos(current_state)):
                 next_state_tuple = turn_grid_to_tuple(next_state)
-                #calcula o custo e a prioridade total do proximo estado
-                g_score = open_set.get(next_state_tuple.float("inf"))+1
-                f_score = self.count_wrong_pieces(next_state) + g_score
+                g_score = float("inf")
+                if next_state_tuple in open_set:
+                    g_score = open_set[next_state_tuple] + 1
+                else:
+                    g_score = float("inf") + 1
+                f_score = self.count_wrong_pieces(next_state, objective_state) + g_score
 
-                if next_state_tuple not in closet_set or f_score<g_score:
-                    # reconstruir o caminho da solução posteriormente, rastreando os pais de cada estado até o estado inicial
-                    parents_dict[next_state_tuple] = current_state
-                    open_set[next_state_tuple] = f_score
+            if next_state_tuple not in closet_set or f_score < g_score:
+                print("rinoceronte")
+                continue
+            parents_dict[next_state_tuple] = current_state
+            open_set[next_state_tuple] = f_score
+            print("hipotamo")
+        
 
-
-
-
+if (__name__ == "__main__"):
+    solver = AWrongPcs_solver()
+    
+    objective_state = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 0]
+    ]
+    
+    initial_state = [
+        [1, 2, 3],
+        [4, 6, 0],
+        [7, 5, 8]
+    ]
+    
+    solution_path = solver.solve_A_WrongPcs(initial_state, objective_state)
+    print(solution_path)
+    print('elefante')
